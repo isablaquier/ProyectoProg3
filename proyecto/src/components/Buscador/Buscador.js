94c9c008e9363d150793f1488272ada4a2f64273
isablaquier <iblaquier@udesa.edu.ar>
2023-04-02 20:00:18 -0300

    intente arrancar buscador

---
+++ proyecto/src/components/Buscador/Buscador.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react'
+class Buscador extends Component{
+    render(){
+        return(
+            <form>
+                <label> Busca lo que quieras</label>
+                <input/>
+                <button> Enviar info</button>
+            </form>
+        )
+    }
+}
+export default Buscador
