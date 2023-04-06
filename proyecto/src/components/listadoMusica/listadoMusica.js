3e7a1a3aa87e2a27a2582e99b0d578f90a8fedd0
isablaquier <iblaquier@udesa.edu.ar>
2023-04-06 09:18:13 -0300

    termine favoritos

--- proyecto/src/components/listadoMusica/listadoMusica.js
+++ proyecto/src/components/listadoMusica/listadoMusica.js
@@ -5,7 +5,7 @@ class ListadoMusica extends Component {
     constructor (props){
         super(props) 
         this.state = {
-           
+           data:props.infoCancion
         }
 
     }
@@ -24,6 +24,38 @@ class ListadoMusica extends Component {
             })
         }
     }
+    addFavourites(){
+        if(localStorage.getItem('favoritos')){
+            let info = this.state.data
+            let favoritos = JSON.parse(localStorage.getItem('favoritos'))
+            favoritos.push(info)
+            let arrayString= JSON.stringify(favoritos)
+            localStorage.setItem('favoritos',arrayString)
+        }else if(!localStorage.getItem('favoritos')){
+            let favoritos = [];
+            let info= this.state.data
+            favoritos.push(info)
+            let arrayString= JSON.stringify(favoritos)
+            localStorage.setItem('favoritos',arrayString)
+        }
+       
+    }
+    removeItem(){
+
+        if(localStorage.getItem('favoritos')){
+            let info= this.state.data
+            let favoritos = JSON.parse(localStorage.getItem('favoritos'))
+            let favoritosRemove= favoritos.filter(canFav => canFav.title !== info.title)
+ if(favoritos.length ==  favoritosRemove.length){
+    alert('No tienes estas cancion en favoritos')
+}else {
+    let favoritosRemoveString = JSON.stringify(favoritosRemove)
+    localStorage.setItem('favoritos', favoritosRemoveString)
+
+}
+        }
+    }
+
     render(){
         return(
         <article>
@@ -33,6 +65,9 @@ class ListadoMusica extends Component {
             <p className='more'>Ver más</p> 
             <section className='extra'>
             </section>
+            <button classNAME= 'boton' onClick={item => this.addFavourites()}>Añadir a Favoritos</button>
+            <button classNAME= 'boton' onClick={item => this.removeItem()}> Eliminar a Favoritos</button>
+
             <p className='delete'>Borrar</p>
             </article>
 
--- proyecto/src/screens/Favoritos/Favoritos.js
+++ proyecto/src/screens/Favoritos/Favoritos.js
@@ -2,13 +2,35 @@ import React, {Component} from 'react'
 import ContenedorListado from '../../components/ContenedorListado/ContenedorListado'
 class Favoritos extends Component {
     constructor(props){
-
+super(props)
+this.state={
+    favoritos:[]
+}
+    }
+    componentDidMount(){
+        let storage = localStorage.getItem('favoritos')
+        if(storage !==null){
+            let storageAArray = JSON.parse(storage)
+            Promise.all(
+                storageAArray.map(id => {
+                    return(
+                        fetch(`https://cors-anywhere.herokuapp.com/https://api.deezer.com/chart/tracks`)
+                        .then(resp => resp.json())
+                        .then(data => data)
+                    )
+                })
+            )
+            .then(data => this.setState({
+                favoritos: data
+            }))
+            .catch(err => console.log(err))
+        }
     }
     render(){
         return (
             
             <>
-            <ContenedorListado />
+            <ContenedorListado info={this.state.favoritos}/>
             </>
         )
     }
